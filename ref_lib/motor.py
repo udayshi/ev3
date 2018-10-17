b755e93060cb8c4d5060bc1a092c47cab3410db0
Uday Shiwakoti <uday.shiwakoti@mullenlowegroup.com>
2018-10-17 15:11:36 +0100

    Changes on wait

--- ref_lib/motor.py
+++ ref_lib/motor.py
@@ -10,10 +10,10 @@ m.run_to_rel_pos(position_sp=360, speed_sp=900, stop_action="hold")
 sleep(5)   # Give the motor time to move
 
 
-m.run_timed(time_sp=3000, speed_sp=-750)
+m.run_timed(time_sp=3000, speed_sp=-750).wait()
+print("set speed (speed_sp) = " + str(m.speed_sp))
 sleep(1)  # it takes a moment for the motor to start moving
 print("actual speed = " + str(m.speed))
-print("set speed (speed_sp) = " + str(m.speed_sp)).wait()
 sleep(4)
 
 
